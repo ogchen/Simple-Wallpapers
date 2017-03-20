8bc58f87d7e8b89543b82e8253ecdc8245c0864a
Oscar <oscarguoyinchen@gmail.com>
2017-03-20 15:28:39 +0000

    Fixed bug with incorrect url and wrote bash script to run process in the background

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ import json
 import os
 import time
 from apscheduler.schedulers.blocking import BlockingScheduler
+index = 0
 
 def change_wallpaper():
+
+    global index
+
     if os.path.isfile('image.jpg'):
         os.remove('image.jpg')
 
-    r = requests.get('https://www.reddit.com/r/wallpaper+wallpapers/hot/.json', headers = {'User-agent': 'Simple Wallpapers bot'})
+    r = requests.get('https://www.reddit.com/r/wallpaper+wallpapers/top/.json', headers = {'User-agent': 'Simple Wallpapers bot'})
     try:
         r.raise_for_status()
     except Exception as exc:
         print('Error accessing Reddit API: %s' % (exc))
 
     data = r.json()
-    url = data['data']['children'][0]['data']['url']
-    url = url.replace('amp;', '')
-
+    url = data['data']['children'][index]['data']['preview']['images'][0]['source']['url']
 
     image = requests.get(url)
     try:
@@ -26,7 +28,7 @@ def change_wallpaper():
         print('Error accessing image: %s' % (exc))
 
     with open('image.jpg', 'wb') as handler:
-        handler.write(image.content)
+            handler.write(image.content)
 
     os.system("./SimpleWallpapers")
 
@@ -37,7 +39,11 @@ def change_wallpaper():
     os.system("gsettings set org.gnome.desktop.background picture-uri file://" + dir_path + "/image.jpg") #Background does not always change without this line
     os.system("gsettings set org.gnome.desktop.background picture-uri file://" + dir_path + "/wallpaper.jpg")
 
+    index += 1
+    if index > 9:
+        index = 0
+
 change_wallpaper()
 scheduler = BlockingScheduler()
-scheduler.add_job(change_wallpaper, 'interval', hours=0.01)
+scheduler.add_job(change_wallpaper, 'interval', hours=2)
 scheduler.start()
---
+++ wallpaper.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+nohup python3 main.py > my.log 2>&1 &
+echo $! > save_pid.txt
+
